023807b56e4975ea5a6851ba138e6b6757433464
pablosambuco <pablosambuco@gmail.com>
2020-10-31 09:40:56 -0300

    Ajustes a custom_functions.py

--- custom_functions.py
+++ custom_functions.py
@@ -4,44 +4,75 @@ import zeroconf
 import time
 from uuid import UUID
 
-def show_status(listener,status):
-#Todo esto deberia ir a una base de datos... sqlite? 
+def write_status(listener,status):
 #Despues ver como sacar los datos por separado de cada chromecast
 #Ver como levantar en vivo a una pagina
 #Agregar botones de control para cada chromecast, play, pausa, stop, volumen (slider)
-    print("-------------------------------------")
-    print("Cast: " + str(listener.cast.device.friendly_name))
+    cast=None
+    estado=None
+    volumen=None
+    mute=None
+    titulo=None
+    subtitulo=None
+    serie=None
+    temporada=None
+    episodio=None
+    artista=None
+    album=None
+    pista=None
+    texto=None
+    icono=None
+    imagen=None
+
+    cast=str(listener.cast.device.friendly_name)
+
     if(hasattr(status,'player_state')):
-       print("Estado: " + status.player_state)
+       estado=status.player_state
     if(hasattr(status,'volume_level')):
-       print("Volumen: " + "{0:.0%}".format(status.volume_level))
+       volumen=status.volume_level
     if(hasattr(status,'volume_muted')):
-       print("Mute: " + str(status.volume_muted))
+       mute=status.volume_muted
     if(hasattr(status,'title')):
-       print("Titulo: " + str(status.title))
+       titulo=status.title
+    if(hasattr(status,'media_metadata')):
+       if('subtitle' in status.media_metadata):
+          subtitulo=status.media_metadata['subtitle']
     if(hasattr(status,'series_title')):
-       print("Serie: " + str(status.series_title))
+       serie=status.series_title
     if(hasattr(status,'season')):
-       print("Temporada: " + str(status.season))
+       temporada=status.season
     if(hasattr(status,'episode')):
-       print("Episodio: " + str(status.episode))
+       episodio=status.episode
     if(hasattr(status,'artist')):
-       print("Artista: " + str(status.artist))
+       artista=status.artist
     if(hasattr(status,'album_name')):
-       print("Album: " + str(status.album_name))
-    if(hasattr(status,'album_artist')):
-       print("Artista: " + str(status.album_artist))
+       album=status.album_name
     if(hasattr(status,'track')):
-       print("Pista: " + str(status.track))
+       pista=status.track
     if(hasattr(status,'status_text')):
-       print("Texto: " + str(status.status_text))
+       texto=status.status_text
     if(hasattr(status,'icon_url')):
-       print("Icono: " + str(status.icon_url))
+       icono=status.icon_url
     if(hasattr(status,'images')):
        if(len(status.images)>=1):
           if(hasattr(status.images[0],'url')):
-             print("Imagen: " + str(status.images[0].url))
+             imagen=status.images[0].url
 
+    print("cast     : " + str(cast      ))
+    print("estado   : " + str(estado    ))
+    print("volumen  : " + str(volumen   ))
+    print("mute     : " + str(mute      ))
+    print("titulo   : " + str(titulo    ))
+    print("subtitulo: " + str(subtitulo ))
+    print("serie    : " + str(serie     ))
+    print("temporada: " + str(temporada ))
+    print("episodio : " + str(episodio  ))
+    print("artista  : " + str(artista   ))
+    print("album    : " + str(album     ))
+    print("pista    : " + str(pista     ))
+    print("texto    : " + str(texto     ))
+    print("icono    : " + str(icono     ))
+    print("imagen   : " + str(imagen    ))
 
 
 class StatusListener:
@@ -50,7 +81,7 @@ class StatusListener:
         self.cast = cast
 
     def new_cast_status(self, status):
-        show_status(self, status)
+        write_status(self, status)
 
 class StatusMediaListener:
     def __init__(self, name, cast):
@@ -58,7 +89,7 @@ class StatusMediaListener:
         self.cast = cast
 
     def new_media_status(self, status):
-        show_status(self, status)
+        write_status(self, status)
 
 dispositivos=[]
 ips=[]
