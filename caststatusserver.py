41a29cfdbf23c1eba96cbf88b88f230e9aa6acc6
pablosambuco <pablosambuco@gmail.com>
2020-11-27 09:30:35 -0300

    new TODO

--- caststatusserver.py
+++ caststatusserver.py
@@ -329,6 +329,7 @@ def get_attribs(listener_type, status):
     #  3: MusicTrackMediaMetadata: title, albumName, artist, images
     #  4: PhotoMediaMetadata: title, artist, location
     #
+    
     # TODO Determinar que comandos estan permitidos (atributo supportedMediaCommands) para enviar al frontend que botones deben estar disponibles
     #  MediaStatus: playerState, supportedMediaCommands, volume
     #
