fd3afe35332fdf71dc48eb63a38247a5c575b0e2
pablosambuco <pablosambuco@gmail.com>
2021-06-05 16:10:54 -0300

    Login mas o menos funcional

---
+++ views/login.tpl
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Cast Status</title>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta name="description" content="Cast Status" />
+    <meta name="author" content="Pablo Sambuco" />
+    <link rel="icon" type="image/png" href="/images/favicon.png" />
+    <link rel="stylesheet" type="text/css" href="/static/login.css" />
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/require.js/2.3.6/require.min.js"></script>
+    <script src="static/main.js"></script>
+</head>
+
+<body>
+    <div id="container">
+        <section class="ftco-section">
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-md-6 text-center mb-5">
+                        <h2 class="heading-section">CastStatusServer</h2>
+                    </div>
+                </div>
+                <div class="row justify-content-center">
+
+                    <div class="col-md-6 col-lg-4">
+                        <div class="login-wrap p-0">
+                            <h3 class="mb-4 text-center">Login</h3>
+                            <form action="/" method="post" class="signin-form">
+                                <div class="form-group">
+                                    <input name="username" class="form-control" placeholder="user" type="text" />
+                                </div>
+                                <div class="form-group">
+                                    <input name="password" class="form-control" placeholder="pass" type="password" />
+                                </div>
+                                <div class="form-group">
+                                    <input value="Login" class="form-control" type="submit" />
+                                </div>
+                                <input name="fn" type="hidden" value="{{filename}}" />
+                            </form>
+                            <p class="rojo w-100 text-center">{{error}}</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </div>
+</body>
+
+</html>
--- web.py
+++ web.py
@@ -1,31 +1,30 @@
 #!/usr/bin/python3
-'''Web.py: servidor principal de CastStatus
-'''
+"""Web.py: servidor principal de CastStatus
+"""
 
-# pylint: disable=line-too-long,fixme
+# pylint: disable=line-too-long,fixme,E1101
 
 import os
 from pathlib import Path
 import logging
 from logging.handlers import RotatingFileHandler
+from base64 import b64decode
 from gevent.pywsgi import WSGIServer
 from geventwebsocket import WebSocketError
 from geventwebsocket.handler import WebSocketHandler
-from bottle import Bottle
-from bottle import static_file
-from bottle import request
-from bottle import abort
+from bottle import Bottle, static_file, request, abort, template
 from werkzeug.debug import DebuggedApplication
 from caststatusserver import CastStatusServer
 
+
 CASTSTATUS = CastStatusServer()
 
-Path('logs').mkdir(parents=True, exist_ok=True)
+Path("logs").mkdir(parents=True, exist_ok=True)
 LOGGER = logging.getLogger()
 HANDLER = RotatingFileHandler(
-    'logs/web.log', maxBytes=1024 * 1024, backupCount=5
+    "logs/web.log", maxBytes=1024 * 1024, backupCount=5
 )
-FORMATTER = logging.Formatter('%(levelname)s %(asctime)s %(message)s')
+FORMATTER = logging.Formatter("%(levelname)s %(asctime)s %(message)s")
 HANDLER.setFormatter(FORMATTER)
 LOGGER.addHandler(HANDLER)
 LOGGER.setLevel(logging.INFO)
@@ -33,94 +32,96 @@ LOGGER.setLevel(logging.INFO)
 DIRNAME = os.path.dirname(os.path.realpath(__file__))
 APP = Bottle()
 
-@APP.route(r'/static/<filename:re:.*\.css>')
+PASSFILE = os.path.join(DIRNAME, "user.pass")
+
+
+@APP.get(r"/static/<filename:re:.*\.css>")
 def send_css(filename):
-    '''Redireccion de static/*.css a static/css/*.css
+    """Redireccion de static/*.css a static/css/*.css
 
     Args:
         filename (string): Ruta del css a redirigir
 
     Returns:
         string Ruta del css redirigido
-    '''
-    root = DIRNAME + '/static/css'
+    """
+    root = DIRNAME + "/static/css"
     return static_file(filename, root=root)
 
 
-@APP.route(r'/static/<filename:re:.*\.js>')
+@APP.get(r"/static/<filename:re:.*\.js>")
 def send_js(filename):
-    '''Redireccion de static/*.js a static/js/*.js
+    """Redireccion de static/*.js a static/js/*.js
 
     Args:
         filename (string): Ruta del js a redirigir
 
     Returns:
         string Ruta del js redirigido
-    '''
-    root = DIRNAME + '/static/js'
+    """
+    root = DIRNAME + "/static/js"
     return static_file(filename, root=root)
 
 
-@APP.route(r'/images/<filename:re:.*\.jpg>')
+@APP.get(r"/images/<filename:re:.*\.jpg>")
 def send_jpg(filename):
-    '''Redireccion de images/*.jpg a static/images/*.jpg
+    """Redireccion de images/*.jpg a static/images/*.jpg
 
     Args:
         filename (string): Ruta del jpg a redirigir
 
     Returns:
         string Ruta del jpg redirigido
-    '''
-    root = DIRNAME + '/static/images'
+    """
+    root = DIRNAME + "/static/images"
     return static_file(filename, root=root)
 
 
-@APP.route(r'/images/<filename:re:.*\.png>')
+@APP.get(r"/images/<filename:re:.*\.png>")
 def send_png(filename):
-    '''Redireccion de images/*.png a static/images/*.png
+    """Redireccion de images/*.png a static/images/*.png
 
     Args:
         filename (string): Ruta del png a redirigir
 
     Returns:
         string Ruta del png redirigido
-    '''
-    root = DIRNAME + '/static/images'
+    """
+    root = DIRNAME + "/static/images"
     return static_file(filename, root=root)
 
 
-@APP.route('/')
-@APP.route('/<filename>')
-def index(filename='index.html'):
-    '''Ruta /
+def index(filename):
+    """Ruta /
 
     Correspode a la pagina principal de la aplicacion
 
     Returns:
         HTML: contenido procesado a partir del template
-    '''
+    """
     # TODO Separar 100% el webserver del CastStatusServer. Ofrecer websocket desde el Cast.
     #  Esto implica sacar las variables del template de index, y crear una funcion en js para dibujar todo desde cero. ver a que nivel hay que insertar los objetos
-    root = DIRNAME + '/static/html'
+    root = DIRNAME + "/static/html"
     return static_file(filename, root=root)
 
 
-@APP.route('/websocket')
+@APP.route("/websocket")
 def handle_websocket():
-    '''Ruta WS /websocket
+    """Ruta WS /websocket
 
     Ruta utilizada para la comunicacion entre JavaScript (AJAX/JQuery) y Python
 
-    '''
-    wsock = request.environ.get('wsgi.websocket')
+    """
+    wsock = request.environ.get("wsgi.websocket")
     if not wsock:
-        abort(400, 'Expected WebSocket request.')
+        abort(400, "Expected WebSocket request.")
 
     while True:
         try:
             CASTSTATUS.atender(wsock)
         except WebSocketError:
             break
+
 
 # @APP.route('/doc')
 # def handle_doc():
@@ -132,6 +133,39 @@ def handle_websocket():
 #     return static_file('index.html', root=DIRNAME+'/html')
 
 
-SERVER = WSGIServer(('192.168.1.40', 8083), DebuggedApplication(APP), handler_class=WebSocketHandler)
+@APP.get("/")
+@APP.get("/<filename>")
+def login(filename="index.html", *, error=""):
+    """Ruta raiz,  formulario de login"""
+    return template("login", filename=filename, error=error)
+
+
+@APP.post("/")
+def do_login():
+    """Post para el login"""
+    username = request.forms.get("username")
+    password = request.forms.get("password")
+    filename = request.forms.get("fn")
+    if check_login(username, password):
+        return index(filename)
+    return login(filename, error="Invalid username/password")
+
+
+def check_login(username, password):
+    """Valida el login contra user.pass si existe"""
+    params = bytes(username + ":" + password, encoding="UTF-8")
+
+    userpass = "dXNlcjpwYXNz"  # default: user:pass
+    if os.path.isfile(PASSFILE):
+        with open(PASSFILE, "r") as passfile:
+            userpass = str(passfile.readline())  # solo leo la primera linea
+    return params == b64decode(userpass)
+
+
+SERVER = WSGIServer(
+    ("192.168.1.40", 8083),
+    DebuggedApplication(APP),
+    handler_class=WebSocketHandler,
+)
 SERVER.serve_forever()
-#APP.run(host='192.168.1.40', port=8083)
+# APP.run(host='192.168.1.40', port=8083)
